a9af9f74380ed08ad0760622399ac50f578e7801
vishalrajput12 <vishalchauhan12.vr@gmail.com>
2022-06-22 12:55:35 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My-First-App';
+  constructor(){
+    // setTimeout(()=>{
+    //   this.title="New Title";
+    // },2000);
+  }
+}
